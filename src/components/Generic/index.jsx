a84a9f9008100be9eb934be94a44f297062cb229
azmu0520 <azizbekbootcam@gmail.com>
2023-01-18 18:48:47 +0500

    done

--- src/components/Generic/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Generic = () => {
-  return <div>Generic</div>;
-};
-
-export default Generic;
--- src/components/Home/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Wrap } from './style';
-
-const Home = () => {
-  return <Wrap>Home</Wrap>;
-};
-
-export default Home;
